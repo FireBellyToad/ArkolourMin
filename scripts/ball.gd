ef9ccee5881ef77111058038b9ca501d45290b98
Faust90 <jacopo.buttiglieri@gmail.com>
2016-05-22 18:11:23 +0200

    Switched to 2D physics, added ball bouncing control

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,17 +1,28 @@
 
-extends RigidBody
+extends RigidBody2D
 
-const INITIAL_SPEED = 10
+const INITIAL_SPEED = 350
 var speed
 
 func _ready():
 	
-	
 	_init_ball()
+	set_fixed_process(true)
 	pass
 	
 func _init_ball():
-	#TODO cambiare
 	speed = INITIAL_SPEED	
-	set_linear_velocity(Vector3(0,-speed,0))
+	set_linear_velocity(Vector2(0,-speed))
 
+func _fixed_process(delta):
+	
+	var bodies = get_colliding_bodies()
+	
+	for body in bodies:
+		
+		#Se l'oggetto con cui collide è la Sbarra
+		if (body.get_name() == "Bat"):
+			#Rimbalza in un angolo che 
+			var direction = get_pos() - body.get_node("Ancor").get_global_pos()
+			var velocity = direction.normalized()*speed
+			set_linear_velocity(velocity)
--- scripts/bat.gd
+++ scripts/bat.gd
@@ -1,23 +1,21 @@
 
-extends KinematicBody
+extends KinematicBody2D
 
 #Bat speed
-const BAT_SPEED = 10
+const BAT_SPEED = 250
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	set_fixed_process(true)
+	set_process(true)
 	pass
 	
-func _fixed_process(delta):
-	
+func _process(delta):	
 	_move_bat(delta)
 
+#Gestione movimento Sbarra
 func _move_bat(delta):
 	
 	if(Input.is_action_pressed("bat_right")):
-		move(Vector3(BAT_SPEED*delta,0,0))
+		move(Vector2(BAT_SPEED*delta,0))
 
 	if(Input.is_action_pressed("bat_left")):
-		move(Vector3(-BAT_SPEED*delta,0,0))
+		move(Vector2(-BAT_SPEED*delta,0))
